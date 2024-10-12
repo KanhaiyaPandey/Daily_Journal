89e89a8341c95db8f5ce1e717da94ed7bad00a47
kanhaiyaPandey <kanhaiyaPandey2232@gmail.com>
2024-10-12 19:55:49 +0530

    adding lombok in the project

--- src/main/java/net/croods/journalApp/entity/JournalEntry.java
+++ src/main/java/net/croods/journalApp/entity/JournalEntry.java
@@ -6,10 +6,13 @@ package net.croods.journalApp.entity;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
 import java.time.LocalDateTime;
 
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
     
     @Id
@@ -20,39 +23,5 @@ public class JournalEntry {
     private String content;
 
     private LocalDateTime  date;
-
-    public LocalDateTime  getDate(){
-        return date;
-    }
-   
-    public void setDate(LocalDateTime  date){
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    // Getter and setter for title
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    // Getter and setter for content
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 
 }
